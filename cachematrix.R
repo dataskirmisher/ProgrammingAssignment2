4f6a9ed6961adc82b6072ea32a4c851886de64d1
dataskirmisher <victorsousa@protonmail.com>
2018-08-08 20:05:49 -0300

    cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,15 @@
+##  R Programming - COURSERA
+##  Programing Assignment 2
+##  Victor Sousa - 08/18
 
-
-## makeCacheMatrix
+## makeCacheMatrix ()
 ## This function creates a special "matrix" object that can cache its inverse.
-
+## It contains a list of methods
+## Set and Get the value of the Matrix and the Inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     
     ## Initialize the inverse matrix (imv) as NULL
-    
     imv <- NULL
     
     ## "set" is a method to replace the stored matrix
@@ -15,7 +17,6 @@ makeCacheMatrix <- function(x = matrix()) {
     set <- function(y){
         x <<- y
         imv <<- NULL
-        
     }
     
     ## "get" is a method to read the value of the matrix
@@ -24,26 +25,51 @@ makeCacheMatrix <- function(x = matrix()) {
     }
     
     ## set the value of the inverse 
-    setimatrix <- function(imatrix) {
+    setimv <- function(imatrix) {
         imv<<-imatrix
     }
     
     ## Method to read the stored imv
-    getimatrix <- function() {
+    getimv <- function() {
         imv
     }
     
     ## returning the methods as list.
-    list(set=set,get=get,setimatrix=setimatrix,getimatrix=getimatrix)
-    
-    
+    list(set=set,get=get,setimv=setimv,getimv=getimv)
 
 }
 
 
-
-## Write a short comment describing this function
+##  cacheSolve ()
+##  This function computes the inverse of the special "matrix" 
+##  returned by makeCacheMatrix above. If the inverse has already
+##  been calculated (and the matrix has not changed), then the 
+##  cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    imv <- x$getimv
+    
+    ## if inverse matrix has been calculated
+    ## retun stored value
+    
+    if (!is.null(imv)){
+        message("getting cached data")
+        return(imv)
+        
+    ## if not, proceed the following
+    }else{
+        ## Read stored matrix
+        data <- x$get()
+        
+        ## Calculate inverse matrix
+        imv <- solve(data,...)
+        
+        ## Store the inverse on cache
+        x$setimv(imv)
+        
+        ## return calculed imv (inverse matrix)
+        imv
+        
+    }
 }
